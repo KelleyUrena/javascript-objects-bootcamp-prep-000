6ceba39fb4c541607bdeee95e36051e6770a242f
kelleynurena@gmail.com Urena <kelleynurena@gmail.com>
2018-08-17 04:00:58 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,6 +1,14 @@
 var playlist = new Object({ Beatles: 'Twist and Shout' })
 
-function updatePlaylist(object, string, song) {
-  
-  return object
+function updatePlaylist(playlist, artist, song) {
+  playlist[artist] = song
+  return playlist
 }
+
+playlist["artist"] = "Hello";
+
+is the same exact thing as...
+
+playlist.artist = "Hello";
+
+
