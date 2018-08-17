cb6d30193648c30905b99c858f269558219ca4fd
kelleynurena@gmail.com Urena <kelleynurena@gmail.com>
2018-08-17 04:03:08 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,14 +1,6 @@
 var playlist = new Object({ Beatles: 'Twist and Shout' })
 
 function updatePlaylist(playlist, artist, song) {
-  playlist[artist] = song
+  playlist[artist] = song;
   return playlist
 }
-
-playlist["artist"] = "Hello";
-
-is the same exact thing as...
-
-playlist.artist = "Hello";
-
-
